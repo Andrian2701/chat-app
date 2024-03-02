62a21ed4454c38d77452b3856151dfcf5a7e7ad7
Andrian Smetaniuk <andrian.smet@gmail.com>
2024-03-02 16:40:03 +0200

    chore: project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  variable: "--var-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.variable}>{children}</body>
     </html>
   );
 }
